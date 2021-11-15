5934a10579656a32b89958d6581d98a00db05593
Uéliton Camargo <uelitonn6@gmail.com>
2021-11-15 09:15:19 -0300

    Validando arquivo csv

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+const constants = {
+    error: {
+        FILE_LENGTH_ERROR_MESSAGE: 'O comprimento do arquivo e invalido!',
+        FILE_FIELDS_ERROR_MESSAGE: 'As propriedades fornecidas sao invalidas!'
+    }
+}
+
+module.exports = constants
--- src/file.js
+++ src/file.js
@@ -1,19 +1,37 @@
 const { readFile } = require('fs/promises')
 const { join } = require('path')
 
+const { error } = require('./constants')
+
+const DEFAULT_OPTION = {
+    maxLines: 3,
+    fields: [ "id","name","profession","age" ]
+}
+
 class File {
     static async csvtojson(filePath) {
-        const result = await File.getFileContent(filePath)
-        return result
+        const content = await File.getFileContent(filePath)
+        const validation = File.isValid(content)
     }
 
     static async getFileContent(filePath) {
         const fileName = join(__dirname, filePath)
         return (await readFile(fileName)).toString('utf8')
     }
+
+    static isValid(csvString, options = DEFAULT_OPTION) {
+        const [ header, ...fileWithoutHeader ] = csvString.split('\n')
+       const isHeaderValid = header === options.fields.join(',')
+        if(!isHeaderValid) {
+            return {
+                error: 
+            }
+        }
+    }
 }
 
 (async() => {
-    const result = await File.csvtojson('../mocks/threeItems-valid.csv')
+    //const result = await File.csvtojson('../mocks/threeItems-valid.csv')
+    const result = await File.csvtojson('../mocks/fourItems-invalid.csv')
     console.log('result', result)
 })()
